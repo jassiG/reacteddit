8934f2166e6752787bc3ea91ae1d33ee5a764352
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2021-11-29 21:41:18 +0530

    Added Initial Code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+
+let baseLink = "https://source.unsplash.com/random/150x400?sig=";
+
+function RedditCard() {
+  return (
+    <div className="RedditCard">
+      <RedditPostHeader />
+      <RedditPostBody />
+    </div>
+  );
+}
+
+function RedditPostHeader() {
+  return (
+    <div className="RedditPostHeader">
+      <h1>Post Title</h1>
+      <PostDetails />
+    </div>
+  );
+}
+function PostDetails() {
+  return (
+    <div className="PostDetails">
+      <p>By: u/Author</p>
+      <p>18/10/21</p>
+    </div>
+  );
+}
+
+function RedditPostBody() {
+  return (
+    <img className="RedditPostImage" src={baseLink + Math.random() * 1000} alt="React Reddit" />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RedditCard />
   );
 }
 
