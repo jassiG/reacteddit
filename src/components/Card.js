20ae905e89bab535d49c791e90ee78e9e01ce4fd
Jaswant Singh <jaswant_s@ch.iitr.ac.in>
2021-12-01 07:00:01 +0530

    Added Video Support

--- src/components/Card.js
+++ src/components/Card.js
@@ -15,14 +15,21 @@ function RedditCard({ post }) {
 					<p>{Date(post.created).toString()}</p>
 				</div>
 			</div>
-
-			<div className="ImageContainer">
-				<img className="RedditPostImage" src={
-					post.url_overridden_by_dest != null
-						? post.url_overridden_by_dest
-						: "https://images.unsplash.com/photo-1584824486509-112e4181ff6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-				} alt="React Reddit" />
-			</div>
+			{
+				post.is_video === true ?
+					<div className="RedditPostVideo">
+						<video controls>
+							<source src={post.media.reddit_video.fallback_url} type="video/mp4" />
+						</video>
+					</div>
+					: <div className="ImageContainer">
+						<img className="RedditPostImage" src={
+							post.url_overridden_by_dest != null
+								? post.url_overridden_by_dest
+								: "https://images.unsplash.com/photo-1584824486509-112e4181ff6b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+						} alt="React Reddit" />
+					</div>
+			}
 			<div className="RedditPostFooter">
 				<p>upvotes: {post.ups}({post.upvote_ratio * 100}%)</p>
 				<p>comments: {post.num_comments}</p>
